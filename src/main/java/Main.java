4ccb94f4baa7abbc5ce2bdf1058c579df8f32f7a
Agshin Guliyev <agshing@Narminas-MacBook-Air.local>
2025-05-12 14:48:50 -0700

    added client response logic

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,31 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args) {
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
+    public static void main(String[] args) {
+        try {
+            ServerSocket serverSocket = new ServerSocket(4221);
 
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
 
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-  }
+            Socket clientSocket = serverSocket.accept();
+            System.out.println("Accepted new connection");
+            System.out.println("Client connected: " + clientSocket.getInetAddress());
+            // BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            String response = "HTTP/1.1 200 OK\\r\\n\\r\\n\n";
+            out.println(response);
+
+            // Close connection
+            clientSocket.close();
+            System.out.println("Client connection closed.");
+
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
 }
